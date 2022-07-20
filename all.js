887e97dbb70bc4e4d3d69e2d4b18bb8ec3f531f3
Eric <t40933777@gmail.com>
2022-07-20 23:25:49 +0800

    add RWD

--- all.js
+++ all.js
@@ -7,3 +7,21 @@
         $(".collapse-content").slideToggle("250");
     });
 });
+
+//Navbar RWD導覽列漢堡Icon
+
+$(document).ready(function() {
+    $('.Showmenu').on('click',function(event) {
+        event.preventDefault();
+        $('body').toggleClass('menu-show');
+        $('.Showmenu').toggleClass('toggle')
+    })
+    $('.nav-list li a').on('click',function(event) {
+        $('body').toggleClass('menu-show');
+        $('.Showmenu').toggleClass('toggle')
+    })
+    // $('.Showmenu').on('click',function(event) {
+    //     event.preventDefault();
+    //     $('body').toggleClass('toggle')
+    // })
+})
